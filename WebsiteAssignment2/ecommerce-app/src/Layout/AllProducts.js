f75777dfad740621356d641d01e6cfffaaebdea3
pratheekshaniveus <pratheeksha.rao@niveussolutions.com>
2023-01-23 10:22:03 +0530

    website 1st commit

---
+++ WebsiteAssignment2/ecommerce-app/src/Layout/AllProducts.js
@@ -0,0 +1,84 @@
+import product1 from "../images/1.png";
+import product2 from "../images/2.png";
+import product3 from "../images/3.png";
+import product4 from "../images/4.png";
+import product5 from "../images/5.png";
+import product6 from "../images/6.webp";
+import product7 from "../images/7.webp";
+import product8 from "../images/8.webp";
+
+export const ALLPRODUCTS = [
+  {
+    id: 1,
+    productName: "IPhone",
+    price: 999.0,
+    productImage: product1,
+    wishlist:"false",
+    count:0,
+    incart:"false"
+  },
+  {
+    id: 2,
+    productName: "Macbook Pro 2022",
+    price: 1999.0,
+    productImage: product2,
+    wishlist:"false",
+    count:0,
+    incart:"false"
+
+  },
+  {
+    id: 3,
+    productName: "Cannon M50 Camera",
+    price: 699.0,
+    productImage: product3,
+    wishlist:"false",
+    count:0,
+    incart:"false"
+  },
+  {
+    id: 4,
+    productName: "WLS Van Gogh Denim Jacket",
+    price: 228.0,
+    productImage: product4,
+    wishlist:"false",
+    count:0,
+    incart:"false"
+  },
+  {
+    id: 5,
+    productName: "LED Light Strips",
+    price: 19.99,
+    productImage: product5,
+    wishlist:"false",
+    count:0,
+    incart:"false"
+  },
+  {
+    id: 6,
+    productName: "SPECTRUM LS TEE",
+    price: 68.0,
+    productImage: product6,
+    wishlist:"false",
+    count:0,
+    incart:"false"
+  },
+  {
+    id: 7,
+    productName: "AUTO SERVICE SHIRT by GOLF WANG",
+    price: 120.0,
+    productImage: product7,
+    wishlist:"false",
+    count:0,
+    incart:"false"
+  },
+  {
+    id: 8,
+    productName: "DON'T TRIP UNSTRUCTURED HAT",
+    price: 40.0,
+    productImage: product8,
+    wishlist:"false",
+    count:0,
+    incart:"false"
+  },
+];
